0816cb7d4db1d2fa1e4ea8301ef923462b2d1ea6
Pedro Henrique Oliveira <pedro.cpt@hotmail.com>
2024-03-18 13:23:54 -0300

    First commit of this study project Java Spring Framework with Swagger documentation

---
+++ src/main/java/dio/webapi/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package dio.webapi.controller;
+
+import dio.webapi.model.Usuario;
+import dio.webapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping()
+    public List<Usuario> getUsers(){
+        return repository.listAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username){
+        return repository.finByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+
+    @PutMapping()
+    public void putUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+}
